2dbd0bfb4f30a843d2fe09268bf0b47421e61fe3
Matt Ingenthron <ingenthr@cep.net>
2018-01-31 22:30:46 -0800

    WIP: add slugishness with DTrace

---
+++ deepcouch.d
@@ -0,0 +1,20 @@
+#!/usr/sbin/dtrace -s
+
+#pragma D option destructive
+#pragma D option quiet
+
+/** On Mac OS, enable dtrace from recovery mode (cmd-R while rebooting) with
+  * csrutil enable --without dtrace
+  */
+
+BEGIN
+{
+printf("%s: inserting delay of %d microseconds.\n", $$0, $1);
+delay = $1 * 1000;
+}
+
+syscall::sendmsg:entry
+/execname == "memcached"/
+{
+chill(delay);
+}
